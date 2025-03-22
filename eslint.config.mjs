1146f156e4638745f463134e8f15747fb4ba58b8
shadab057 <mohdshadab4629@gmail.com>
2025-03-23 03:40:05 +0530

    setup and add shadcn/ui, Dark Mode, and toast

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules : {
+      "react/no-unescapped-entities" : "off",
+    },
+  },
 ];
 
 export default eslintConfig;
---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function toaster() {
+  return (
+    <div></div>
+  )
+}
+
+export default toaster
